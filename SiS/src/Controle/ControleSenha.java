1413f99bd315c49a7a893a9a12945dcbcb4992b2
TnTxPy <lucas1999mendes@gmail.com>
2018-08-10 01:18:05 -0300

    SiS v1.0.0 - 10/08 - Finalização C.A.R.B. - BUSCA POR SENHA / DESC

--- SiS/src/Controle/ControleSenha.java
+++ SiS/src/Controle/ControleSenha.java
@@ -9,9 +9,9 @@ import Conexao.BancoDeDados;
 import Objetos.Senha;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.ArrayList;
 
 /**
  *
@@ -23,9 +23,8 @@ public class ControleSenha {
         Connection con = null;
         PreparedStatement stmt = null;
         con = conex.get();
-        stmt = con.prepareStatement("INSERT INTO senhas (senha,desc) VALUES (?,?) ");
-        stmt.setInt(1, se.getSenha());
-        stmt.setString(2, se.getDesc());
+        stmt = con.prepareStatement("INSERT INTO senhas (des) VALUES (?) ");
+        stmt.setString(1, se.getDesc());
         stmt.execute();
         con.close();
     }
@@ -42,8 +41,40 @@ public class ControleSenha {
         Connection con = null;
         PreparedStatement stmt = null;
         con = conex.get();
-        stmt = con.prepareStatement("DELETE * FROM senhas");
+        stmt = con.prepareStatement("TRUNCATE TABLE senhas");
         stmt.execute();
         conex.close(con, stmt);
+    }
+    public Senha buscar(Senha se) throws SQLException{
+        Connection con = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        con = conex.get();
+        stmt = con.prepareStatement("SELECT * FROM senhas WHERE senha = ?");
+        stmt.setInt(1, se.getSenha());
+        rs = stmt.executeQuery();
+        Senha re = new Senha();
+        re.setSenha(rs.getInt("senha"));
+        re.setDesc(rs.getString("des"));
+        conex.close(con,stmt,rs);
+        return re;
+    }
+    public ArrayList<Senha> buscar(String se) throws SQLException{
+        Connection con = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        con = conex.get();
+        stmt = con.prepareStatement("SELECT * FROM senhas WHERE des = ?");
+        stmt.setString(1, se);
+        rs = stmt.executeQuery();
+        Senha re = new Senha();
+        ArrayList<Senha> senhasR = new ArrayList<Senha>();
+        while(rs.next()){
+            re.setSenha(rs.getInt("senha"));
+            re.setDesc(rs.getString("des"));
+            senhasR.add(re);
+        }
+        conex.close(con,stmt,rs);
+        return senhasR;
     }
 }
